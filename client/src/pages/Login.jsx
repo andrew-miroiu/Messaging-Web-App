eda0bd119fb3007f89c80dc5410e9223adf476cb
Andrei Miroiu <andreimiroiu@Andreis-MacBook-Air.local>
2025-10-30 23:12:49 +0200

    fixed redirecting

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,7 +25,7 @@ export default function Login({ onLogin }) {
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: { redirectTo: window.location.origin },
+        options: { redirectTo: `${window.location.origin}`},
       })
       if (error) throw error
     } catch (error) {
